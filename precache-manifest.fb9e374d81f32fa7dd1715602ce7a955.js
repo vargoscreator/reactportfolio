7ba7ac45b24178636aed9e4495b98e1ab2eee16b
Vasyl <bacahoriyk@gmail.com>
2022-11-01 00:19:39 +0200

    Updates

---
+++ precache-manifest.fb9e374d81f32fa7dd1715602ce7a955.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2736bab7511b1e8a7eb",
+    "url": "/reactportfolio/static/css/main.c2f57405.chunk.css"
+  },
+  {
+    "revision": "d2736bab7511b1e8a7eb",
+    "url": "/reactportfolio/static/js/main.742b09f8.chunk.js"
+  },
+  {
+    "revision": "8007ccf6b0ab45eb369a",
+    "url": "/reactportfolio/static/js/runtime~main.d7794af4.js"
+  },
+  {
+    "revision": "7318ca007f51ac90626e",
+    "url": "/reactportfolio/static/js/2.d95e1ec4.chunk.js"
+  },
+  {
+    "revision": "5318064858062a6ca603f5f315d0575e",
+    "url": "/reactportfolio/static/media/icon-1.53180648.svg"
+  },
+  {
+    "revision": "f150b46aea45eadf0dd27b0610ab6733",
+    "url": "/reactportfolio/static/media/icon-2.f150b46a.svg"
+  },
+  {
+    "revision": "48e93033b5cdee8e6321d6c891b2502e",
+    "url": "/reactportfolio/static/media/icon-3.48e93033.svg"
+  },
+  {
+    "revision": "2b2b7da40e9fe37746226683d444215a",
+    "url": "/reactportfolio/static/media/getFetch.2b2b7da4.cjs"
+  },
+  {
+    "revision": "19019bfe11c524dc9dc7698ec63560fc",
+    "url": "/reactportfolio/static/media/logo.19019bfe.svg"
+  },
+  {
+    "revision": "494ec2243e13125a3a4333c07f6933ab",
+    "url": "/reactportfolio/static/media/Vargos.494ec224.png"
+  },
+  {
+    "revision": "b1e9a5be641f0e26702611f7f57eb344",
+    "url": "/reactportfolio/static/media/slider1.b1e9a5be.webp"
+  },
+  {
+    "revision": "3289e1dd415c247251fb2ce05d6cf08b",
+    "url": "/reactportfolio/static/media/slider2.3289e1dd.webp"
+  },
+  {
+    "revision": "e92537a02b735fcca7da6551111da6da",
+    "url": "/reactportfolio/static/media/slider3.e92537a0.webp"
+  },
+  {
+    "revision": "578f8cf35d7878b3583a57075063158f",
+    "url": "/reactportfolio/static/media/slider4.578f8cf3.webp"
+  },
+  {
+    "revision": "58b8c5e0e34fe4f74e19749ed80c72c4",
+    "url": "/reactportfolio/static/media/slider5.58b8c5e0.webp"
+  },
+  {
+    "revision": "0455d79f7252a018612a6f4c531ba341",
+    "url": "/reactportfolio/static/media/lrb-ua.0455d79f.webp"
+  },
+  {
+    "revision": "68de921aec7c947a6fc587d1ced426aa",
+    "url": "/reactportfolio/static/media/lrb-ru.68de921a.webp"
+  },
+  {
+    "revision": "2b50f707c3ee095eec660670080244a3",
+    "url": "/reactportfolio/static/media/lrb-en.2b50f707.webp"
+  },
+  {
+    "revision": "7fe74ea8bc0e2ad0108a310830905f1d",
+    "url": "/reactportfolio/static/media/notes1.7fe74ea8.webp"
+  },
+  {
+    "revision": "bee8fd138366505e1c9e61bca97ba3e3",
+    "url": "/reactportfolio/static/media/xiaomi-ua.bee8fd13.webp"
+  },
+  {
+    "revision": "64414b371222e5eab6ae9cb25ece2903",
+    "url": "/reactportfolio/static/media/xiaomi.64414b37.webp"
+  },
+  {
+    "revision": "555f6904b054741227d78a0674d43ef7",
+    "url": "/reactportfolio/static/media/happy.555f6904.webp"
+  },
+  {
+    "revision": "c2494ca8916ed1dbf2c471556542c417",
+    "url": "/reactportfolio/static/media/webisode.c2494ca8.webp"
+  },
+  {
+    "revision": "bd6f23fb1adecd724486cd4d5728dc87",
+    "url": "/reactportfolio/static/media/Vector.bd6f23fb.svg"
+  },
+  {
+    "revision": "5be6b163397f1c2e24acb88a85ce31a5",
+    "url": "/reactportfolio/static/media/instagram.5be6b163.svg"
+  },
+  {
+    "revision": "f579b89ca4ecc808f2654c11ba5b76c5",
+    "url": "/reactportfolio/static/media/github.f579b89c.svg"
+  },
+  {
+    "revision": "4eac3b2fa36c565c3f42698ab9356271",
+    "url": "/reactportfolio/static/media/telegram.4eac3b2f.svg"
+  },
+  {
+    "revision": "a4aaff62faabaa35d37fea337a13a72d",
+    "url": "/reactportfolio/static/media/linkedin.a4aaff62.svg"
+  },
+  {
+    "revision": "2a42f1fc6ef2c41da6c8ca3e25ad9109",
+    "url": "/reactportfolio/static/media/upwork.2a42f1fc.svg"
+  },
+  {
+    "revision": "4120ed83d54864bfcc8ed09e3567258b",
+    "url": "/reactportfolio/static/media/freelancer.4120ed83.svg"
+  },
+  {
+    "revision": "66401af2397acd32314c6bd92cf4a3bb",
+    "url": "/reactportfolio/static/media/fiverr.66401af2.svg"
+  },
+  {
+    "revision": "3f81ded750df634fab8e043fe3bd7084",
+    "url": "/reactportfolio/static/media/freelancehunt.3f81ded7.svg"
+  },
+  {
+    "revision": "e2d5d7098a525e81e4ab5a9caf6a8f66",
+    "url": "/reactportfolio/index.html"
+  }
+];
